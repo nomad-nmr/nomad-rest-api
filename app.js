e758c2ec5d13e20e8a883ea45a6dad57049e5054
tomlebl <tl12@st-andrews.ac.uk>
2020-07-22 23:09:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../nmr-control-dash/build')
+const port = process.argv[2] ? process.argv[2] : 3000
+
+app.use(express.static(publicDirectoryPath))
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
+})
