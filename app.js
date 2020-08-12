de26c111bfa60e861bb2bfb99a5f462596981cc9
tomlebl <tl12@st-andrews.ac.uk>
2020-08-12 14:20:09 +0100

    Adding redirect for React router

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../nmr-control-dash/build')
+const publicDirectoryPath = path.join(__dirname, '..', 'nmr-control-dash', 'build')
+
 const port = process.argv[2] ? process.argv[2] : 3000
 
 app.use(express.static(publicDirectoryPath))
 
 app.get('/api/test', (req, res) => {
 	res.send({ test: true })
+})
+
+// Redirecting any requests that don't match with above to be handled by React router
+app.use((req, res, next) => {
+	res.sendFile(path.join(publicDirectoryPath, 'index.html'))
 })
 
 app.listen(port, () => {
