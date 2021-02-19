8f58f593ec10ffa3a70e4c6a55f6a6d303f9b35c
tomlebl <tl12@st-andrews.ac.uk>
2021-02-19 12:44:10 +0000

    update of pending experiment drawer

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -30,28 +30,34 @@ exports.getStatusTable = async (req, res) => {
 
 exports.getDrawerTable = async (req, res) => {
 	try {
-		const data = await Instrument.find({}, 'name status.statusTable status.historyTable')
+		const data = await Instrument.find({}, 'available name status.statusTable status.historyTable')
 		if (!data) {
 			return res.status(404).send()
 		}
 
 		let respArray = []
+		// pending experiments have status "available" in and errors "error" in the source data table
 		const statusId =
 			req.params.id === 'pending' ? 'available' : req.params.id === 'errors' ? 'error' : req.params.id
 
 		data.forEach(i => {
-			const filteredArray = i.status.statusTable
-				.filter(row => row.status.toLowerCase() === statusId.toLowerCase())
-				.map(row => {
-					const histObject = i.status.historyTable.find(
-						i => row.datasetName === i.datasetName && row.expNo === i.expNo
-					)
-					if (histObject) {
-						return { ...row, instrument: i.name, description: histObject.remarks }
-					} else {
-						return { ...row, instrument: i.name }
-					}
-				})
+			let filteredArray = []
+			//getting only "pending" entries for available instruments
+			if (statusId !== 'available' || (statusId === 'available' && i.available)) {
+				filteredArray = i.status.statusTable
+					.filter(row => row.status.toLowerCase() === statusId.toLowerCase())
+					.map(row => {
+						const histObject = i.status.historyTable.find(
+							i => row.datasetName === i.datasetName && row.expNo === i.expNo
+						)
+						if (histObject) {
+							return { ...row, instrument: i.name, description: histObject.remarks }
+						} else {
+							return { ...row, instrument: i.name }
+						}
+					})
+			}
+
 			respArray = respArray.concat(filteredArray)
 		})
 
