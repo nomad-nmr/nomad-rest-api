f3542d5438a68e2b3c1624c91774423c9e50bd41
Tomas Lebl <tl12@st-andrews.ac.uk>
2021-05-26 12:31:57 +0100

    nodemailer config fix

--- utils/emailTransporter.js
+++ utils/emailTransporter.js
@@ -5,8 +5,10 @@ module.exports = nodemailer.createTransport({
 	port: +process.env.SMTP_PORT,
 	secure: process.env.SMTP_SECURE === 'true',
 	requireTLS: process.env.SMTP_REQUIRE_TLS === 'true',
-	auth: {
-		user: process.env.SMTP_USER,
-		pass: process.env.SMTP_PASS
-	}
+	auth: process.env.SMTP_USER
+		? {
+				user: process.env.SMTP_USER,
+				pass: process.env.SMTP_PASS
+		  }
+		: null
 })
