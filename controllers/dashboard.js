406f8845f4dbe491c182d2a87751e0677381f463
Tomas Lebl <tomlebl44@gmail.com>
2021-08-30 12:00:22 +0100

    Pending filter

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,158 +1,174 @@
 const Instrument = require('../models/instrument')
+const Group = require('../models/group')
 const moment = require('moment')
 const momentDurationFormatSetup = require('moment-duration-format')
 
 exports.getStatusSummary = async (req, res) => {
-	try {
-		const data = await Instrument.find({ isActive: true }, '-status.statusTable -status.historyTable')
-		if (!data) {
-			return res.status(404).send()
-		}
-		res.send(data)
-	} catch (error) {
-		res.status(500).send(error)
-	}
+  try {
+    const data = await Instrument.find(
+      { isActive: true },
+      '-status.statusTable -status.historyTable'
+    )
+    if (!data) {
+      return res.status(404).send()
+    }
+    res.send(data)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 }
 
 exports.getStatusTable = async (req, res) => {
-	try {
-		let data = null
-		//instrId is unknown and set to 0 when  the dashboard reloads
-		if (req.params.instrId !== '0') {
-			data = await Instrument.findById(req.params.instrId, 'status.statusTable')
-		} else {
-			const instrArr = await Instrument.find({ isActive: 'true' }, 'status.statusTable')
-			data = instrArr[0]
-		}
-		if (!data) {
-			return res.status(404).send()
-		}
+  try {
+    let data = null
+    //instrId is unknown and set to 0 when  the dashboard reloads
+    if (req.params.instrId !== '0') {
+      data = await Instrument.findById(req.params.instrId, 'status.statusTable')
+    } else {
+      const instrArr = await Instrument.find({ isActive: 'true' }, 'status.statusTable')
+      data = instrArr[0]
+    }
+    if (!data) {
+      return res.status(404).send()
+    }
 
-		//Filtering off entries in the table with 'Available'(Pending) status
-		const filteredData = data.status.statusTable.filter(entry => entry.status !== 'Available')
+    //Filtering off entries in the table with 'Available'(Pending) status
+    const filteredData = data.status.statusTable.filter(entry => entry.status !== 'Available')
 
-		//Creating data structure for nested expandable tables
-		const tableData = []
-		let newRow = { exps: [] }
-		filteredData.forEach((row, index) => {
-			const prevRow = filteredData[index - 1]
-			const nextRow = filteredData[index + 1]
+    //Creating data structure for nested expandable tables
+    const tableData = []
+    let newRow = { exps: [] }
+    filteredData.forEach((row, index) => {
+      const prevRow = filteredData[index - 1]
+      const nextRow = filteredData[index + 1]
 
-			const newExp = {
-				key: row.expNo,
-				expNo: row.expNo,
-				parameterSet: row.parameterSet,
-				parameters: row.parameters,
-				title: row.title,
-				expT: row.time,
-				status: row.status,
-				updatedAt: row.updatedAt
-			}
+      const newExp = {
+        key: row.expNo,
+        expNo: row.expNo,
+        parameterSet: row.parameterSet,
+        parameters: row.parameters,
+        title: row.title,
+        expT: row.time,
+        status: row.status,
+        updatedAt: row.updatedAt
+      }
 
-			if (index === 0 || prevRow.datasetName !== row.datasetName) {
-				newRow = { exps: [] }
-				newRow.key = row.holder
-				newRow.holder = row.holder
-				newRow.username = row.username
-				newRow.group = row.group
-				newRow.datasetName = row.datasetName
-				newRow.solvent = row.solvent
-				newRow.time = row.time
-				newRow.night = row.night
-				newRow.status = row.status
-				newRow.submittedAt = row.submittedAt
-				newRow.exps = []
-			} else {
-				newRow.time = moment
-					.duration(newRow.time)
-					.add(moment.duration(row.time))
-					.format('HH:mm:ss', { trim: false })
+      if (index === 0 || prevRow.datasetName !== row.datasetName) {
+        newRow = { exps: [] }
+        newRow.key = row.holder
+        newRow.holder = row.holder
+        newRow.username = row.username
+        newRow.group = row.group
+        newRow.datasetName = row.datasetName
+        newRow.solvent = row.solvent
+        newRow.time = row.time
+        newRow.night = row.night
+        newRow.status = row.status
+        newRow.submittedAt = row.submittedAt
+        newRow.exps = []
+      } else {
+        newRow.time = moment
+          .duration(newRow.time)
+          .add(moment.duration(row.time))
+          .format('HH:mm:ss', { trim: false })
 
-				if (row.status === 'Error' || row.status === 'Running') {
-					newRow.status = row.status
-				} else if (row.status === 'Submitted' && newRow.status === 'Completed') {
-					newRow.status = 'Submitted'
-				}
-			}
+        if (row.status === 'Error' || row.status === 'Running') {
+          newRow.status = row.status
+        } else if (row.status === 'Submitted' && newRow.status === 'Completed') {
+          newRow.status = 'Submitted'
+        }
+      }
 
-			newRow.exps.push(newExp)
+      newRow.exps.push(newExp)
 
-			if (!nextRow || nextRow.datasetName !== row.datasetName) {
-				tableData.push(newRow)
-			}
-		})
+      if (!nextRow || nextRow.datasetName !== row.datasetName) {
+        tableData.push(newRow)
+      }
+    })
 
-		res.send(tableData)
-	} catch (error) {
-		console.log(error)
-		res.status(500).send(error)
-	}
+    res.send(tableData)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
 }
 
 exports.getDrawerTable = async (req, res) => {
-	try {
-		const data = await Instrument.find({}, '_id available name status.statusTable status.historyTable')
-		if (!data) {
-			return res.status(404).send()
-		}
+  try {
+    const data = await Instrument.find(
+      {},
+      '_id available name status.statusTable status.historyTable'
+    )
+    if (!data) {
+      return res.status(404).send()
+    }
 
-		let respArray = []
-		// pending experiments have status "available" in and errors "error" in the source data table
-		const statusId =
-			req.params.id === 'pending' ? 'available' : req.params.id === 'errors' ? 'error' : req.params.id
+    const batchGroups = await Group.find({ isBatch: true, isActive: true }, 'groupName')
+    const batchGroupsArr = batchGroups.map(group => group.groupName)
 
-		data.forEach(i => {
-			let filteredArray = []
-			let respArrayChunk = []
+    let respArray = []
+    // pending experiments have status "available" in and errors "error" in the source data table
+    const statusId =
+      req.params.id === 'pending'
+        ? 'available'
+        : req.params.id === 'errors'
+        ? 'error'
+        : req.params.id
 
-			filteredArray = i.status.statusTable.filter(
-				row => row.status.toLowerCase() === statusId.toLowerCase()
-			)
+    data.forEach(i => {
+      let filteredArray = []
+      let respArrayChunk = []
 
-			if (req.params.id === 'errors') {
-				respArrayChunk = filteredArray.map(row => {
-					const histObject = i.status.historyTable.find(
-						i => row.datasetName === i.datasetName && row.expNo === i.expNo
-					)
-					if (histObject) {
-						return { ...row, description: histObject.remarks }
-					} else {
-						return row
-					}
-				})
-			} else if (req.params.id === 'pending') {
-				let expCount = 0
-				let newRow = {}
-				filteredArray.forEach((row, index) => {
-					const prevRow = filteredArray[index - 1]
-					const nextRow = filteredArray[index + 1]
+      filteredArray = i.status.statusTable.filter(
+        row => row.status.toLowerCase() === statusId.toLowerCase()
+      )
 
-					if (index === 0 || prevRow.datasetName !== row.datasetName) {
-						expCount = 1
-					}
-					if (!nextRow || nextRow.datasetName !== row.datasetName) {
-						newRow = {
-							...row,
-							title: row.title.split('||')[0],
-							instrId: i._id,
-							expCount
-						}
-						delete newRow.expNo
-						delete newRow.parameterSet
-						respArrayChunk.push(newRow)
-					}
-					expCount++
-				})
-			} else {
-				respArrayChunk = filteredArray
-			}
+      if (req.params.id === 'errors') {
+        respArrayChunk = filteredArray.map(row => {
+          const histObject = i.status.historyTable.find(
+            i => row.datasetName === i.datasetName && row.expNo === i.expNo
+          )
+          if (histObject) {
+            return { ...row, description: histObject.remarks }
+          } else {
+            return row
+          }
+        })
+      } else if (req.params.id === 'pending') {
+        let expCount = 0
+        let newRow = {}
+        filteredArray
+          .filter(row => !batchGroupsArr.includes(row.group))
+          .forEach((row, index) => {
+            const prevRow = filteredArray[index - 1]
+            const nextRow = filteredArray[index + 1]
 
-			respArray = respArray.concat(respArrayChunk.map(row => ({ ...row, instrument: i.name })))
-		})
+            if (index === 0 || prevRow.datasetName !== row.datasetName) {
+              expCount = 1
+            }
+            if (!nextRow || nextRow.datasetName !== row.datasetName) {
+              newRow = {
+                ...row,
+                title: row.title.split('||')[0],
+                instrId: i._id,
+                expCount
+              }
+              delete newRow.expNo
+              delete newRow.parameterSet
+              respArrayChunk.push(newRow)
+            }
+            expCount++
+          })
+      } else {
+        respArrayChunk = filteredArray
+      }
 
-		res.send(respArray)
-	} catch (error) {
-		console.log(error)
-		res.status(500).send(error)
-	}
+      respArray = respArray.concat(respArrayChunk.map(row => ({ ...row, instrument: i.name })))
+    })
+
+    res.send(respArray)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
 }
--- controllers/tracker/tracker.js
+++ controllers/tracker/tracker.js
@@ -1,57 +1,63 @@
 const io = require('../../socket')
 
 const Instrument = require('../../models/instrument')
+const Group = require('../../models/group')
 const restructureInput = require('./restructureInput')
 const expHistAutoFeed = require('./expHistAutoFeed')
 const updateStatus = require('./updateStatus')
 const app = require('../../app')
 
 exports.ping = async (req, res) => {
-	try {
-		const { name } = await Instrument.findById(req.params.instrumentId, 'name')
-		res.status(200).send({ name })
-	} catch (error) {
-		res.status(500).send(error)
-	}
+  try {
+    const { name } = await Instrument.findById(req.params.instrumentId, 'name')
+    res.status(200).send({ name })
+  } catch (error) {
+    res.status(500).send(error)
+  }
 }
 
 exports.updateStatus = async (req, res) => {
-	try {
-		const instrument = await Instrument.findById(req.body.instrumentId)
+  try {
+    const instrument = await Instrument.findById(req.body.instrumentId)
 
-		if (!instrument) {
-			return res.status(404).send('Instrument not found')
-		}
+    if (!instrument) {
+      return res.status(404).send('Instrument not found')
+    }
 
-		const newStatusObj = restructureInput(req.body.data, instrument)
+    const batchGroups = await Group.find({ isBatch: true, isActive: true }, 'groupName')
+    const batchGroupsArr = batchGroups.map(group => group.groupName)
 
-		if (process.env.SUBMIT_ON === 'true') {
-			const updatedStatusTable = await updateStatus(
-				instrument,
-				newStatusObj.statusTable,
-				newStatusObj.historyTable
-			)
-			newStatusObj.statusTable = updatedStatusTable
-		} else {
-			await expHistAutoFeed(
-				{ id: instrument._id, name: instrument.name },
-				newStatusObj.statusTable,
-				newStatusObj.historyTable
-			)
-		}
+    const newStatusObj = restructureInput(req.body.data, instrument, batchGroupsArr)
 
-		instrument.status = newStatusObj
+    if (process.env.SUBMIT_ON === 'true') {
+      const updatedStatusTable = await updateStatus(
+        instrument,
+        newStatusObj.statusTable,
+        newStatusObj.historyTable
+      )
+      newStatusObj.statusTable = updatedStatusTable
+    } else {
+      await expHistAutoFeed(
+        { id: instrument._id, name: instrument.name },
+        newStatusObj.statusTable,
+        newStatusObj.historyTable
+      )
+    }
 
-		const instr = await instrument.save()
+    instrument.status = newStatusObj
 
-		const submitter = app.getSubmitter()
-		submitter.updateUsedHolders(instr._id.toString(), newStatusObj.statusTable)
+    const instr = await instrument.save()
 
-		io.getIO().to('users').emit('statusUpdate', { instrId: instr._id, statusSummary: instr.status.summary })
+    const submitter = app.getSubmitter()
+    submitter.updateUsedHolders(instr._id.toString(), newStatusObj.statusTable)
 
-		res.status(201).send()
-	} catch (err) {
-		console.log(err)
-		res.status(500).send()
-	}
+    io.getIO()
+      .to('users')
+      .emit('statusUpdate', { instrId: instr._id, statusSummary: instr.status.summary })
+
+    res.status(201).send()
+  } catch (err) {
+    console.log(err)
+    res.status(500).send()
+  }
 }
