a94ad2b43004d8ad15f2d81f072e7fe2ce85d15c
Tomas Lebl <tomlebl44@gmail.com>
2021-08-31 11:36:36 +0100

    batch branch merge saved

--- controllers/tracker/updateStatus.js
+++ controllers/tracker/updateStatus.js
@@ -6,71 +6,67 @@ const expHistAutoFeed = require('./expHistAutoFeed')
 // if expId is not found in expHist table expHistAutoFeed is called to create a new entry in expHist table
 
 const updateStatus = async (instrument, statusTable, historyTable) => {
-	try {
-		const updatedStatusTable = Promise.all(
-			statusTable.map(async entry => {
-				const expId = entry.datasetName + '-' + entry.expNo
-				const oldEntry = instrument.status.statusTable.find(
-					i => i.datasetName === entry.datasetName && i.expNo === entry.expNo
-				)
+  try {
+    const updatedStatusTable = Promise.all(
+      statusTable.map(async entry => {
+        const expId = entry.datasetName + '-' + entry.expNo
+        const oldEntry = instrument.status.statusTable.find(
+          i => i.datasetName === entry.datasetName && i.expNo === entry.expNo
+        )
 
-				//
-				if (!oldEntry || oldEntry.status !== entry.status) {
-					//looking for expHistEntry only if status has changed to reduce number of DB queries
+        //
+        if (!oldEntry || oldEntry.status !== entry.status) {
+          //looking for expHistEntry only if status has changed to reduce number of DB queries
 
-					const historyTableItem = historyTable.find(
-						i => i.datasetName === entry.datasetName && i.expNo === entry.expNo
-					)
+          const historyTableItem = historyTable.find(
+            i => i.datasetName === entry.datasetName && i.expNo === entry.expNo
+          )
 
-					const updateObj = {
-						status: entry.status,
-						expTime: entry.time,
-						remarks: historyTableItem && historyTableItem.remarks,
-						load: historyTableItem && historyTableItem.load,
-						atma: historyTableItem && historyTableItem.atma,
-						spin: historyTableItem && historyTableItem.spin,
-						lock: historyTableItem && historyTableItem.lock,
-						shim: historyTableItem && historyTableItem.shim,
-						proc: historyTableItem && historyTableItem.proc,
-						acq: historyTableItem && historyTableItem.acq
-					}
+          const updateObj = {
+            status: entry.status,
+            expTime: entry.time,
+            remarks: historyTableItem && historyTableItem.remarks,
+            load: historyTableItem && historyTableItem.load,
+            atma: historyTableItem && historyTableItem.atma,
+            spin: historyTableItem && historyTableItem.spin,
+            lock: historyTableItem && historyTableItem.lock,
+            shim: historyTableItem && historyTableItem.shim,
+            proc: historyTableItem && historyTableItem.proc,
+            acq: historyTableItem && historyTableItem.acq
+          }
 
-<<<<<<< HEAD
-					
-=======
->>>>>>> batch
-					//if entry.status === 'Submitted' is used submittedAt is occasionally missing
-					//It might be that entry goes directly to 'Running' status.
-					if (oldEntry) {
-						if (oldEntry.status === 'Available') {
-							updateObj.submittedAt = new Date()
-						}
-					}
+          //if entry.status === 'Submitted' is used submittedAt is occasionally missing
+          //It might be that entry goes directly to 'Running' status.
+          if (oldEntry) {
+            if (oldEntry.status === 'Available') {
+              updateObj.submittedAt = new Date()
+            }
+          }
 
-					const expHistEntry = await Experiment.findOneAndUpdate({ expId }, updateObj)
+          const expHistEntry = await Experiment.findOneAndUpdate({ expId }, updateObj)
 
-					if (expHistEntry) {
-						const { solvent, parameters, night, submittedAt, updatedAt } = expHistEntry
-						return { ...entry, solvent, parameters, night, submittedAt, updatedAt }
-					} else {
-						console.log(`Entry with expId ${expId} not found! AUTO-FEED`)
-						await expHistAutoFeed(
-							{ name: instrument.name, id: instrument._id },
-							statusTable,
-							historyTable
-						)
-						return entry
-					}
-				} else {
-					return oldEntry
-				}
-			})
-		)
+          if (expHistEntry) {
+            const { solvent, parameters, night, submittedAt, updatedAt } = expHistEntry
+            return { ...entry, solvent, parameters, night, submittedAt, updatedAt }
+          } else {
+            console.log(`Entry with expId ${expId} not found! AUTO-FEED`)
+            await expHistAutoFeed(
+              { name: instrument.name, id: instrument._id },
+              statusTable,
+              historyTable
+            )
+            return entry
+          }
+        } else {
+          return oldEntry
+        }
+      })
+    )
 
-		return Promise.resolve(updatedStatusTable)
-	} catch (error) {
-		return Promise.reject(error)
-	}
+    return Promise.resolve(updatedStatusTable)
+  } catch (error) {
+    return Promise.reject(error)
+  }
 }
 
 module.exports = updateStatus
