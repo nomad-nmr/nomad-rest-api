ed8a9bf7954590f56bf926c72ee6587887f8afc8
tomlebl <tl12@st-andrews.ac.uk>
2021-11-15 16:48:36 +0000

    adding new values to instruments

--- models/instrument.js
+++ models/instrument.js
@@ -2,55 +2,61 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const instrumentSchema = new Schema({
-	name: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	model: String,
-	probe: String,
-	capacity: {
-		type: Number,
-		required: true
-	},
-	available: {
-		type: Boolean,
-		default: false
-	},
-	isActive: {
-		type: Boolean,
-		required: true,
-		default: true
-	},
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  model: String,
+  probe: String,
+  capacity: {
+    type: Number,
+    required: true
+  },
+  available: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+    default: true
+  },
 
-	status: {
-		summary: {
-			busyUntil: {
-				type: String,
-				default: 'unknown'
-			},
-			dayExpt: {
-				type: String,
-				default: 'unknown'
-			},
-			nightExpt: {
-				type: String,
-				default: 'unknown'
-			},
-			running: Boolean,
-			availableHolders: Number,
-			errorCount: Number,
-			pendingCount: Number
-		},
-		statusTable: {
-			type: Array,
-			default: []
-		},
-		historyTable: {
-			type: Array,
-			default: []
-		}
-	}
+  dayAllowance: Number,
+  nightAllowance: Number,
+  maxNight: Number,
+  overheadTime: Number,
+
+  status: {
+    summary: {
+      busyUntil: {
+        type: String,
+        default: 'unknown'
+      },
+      dayExpt: {
+        type: String,
+        default: 'unknown'
+      },
+      nightExpt: {
+        type: String,
+        default: 'unknown'
+      },
+      running: Boolean,
+      availableHolders: Number,
+      errorCount: Number,
+      pendingCount: Number
+    },
+    statusTable: {
+      type: Array,
+      default: []
+    },
+    historyTable: {
+      type: Array,
+      default: []
+    }
+  }
 })
 
 module.exports = mongoose.model('Instrument', instrumentSchema)
